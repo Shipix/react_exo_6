beade87eaaffc95a496edb8877d5ecf86a683738
Shipix <shipix@hotmail.fr>
2021-01-20 15:47:18 +0100

    Event click

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 
 export default class Navigation extends Component {
+    link1 = ()=>{
+        console.log("Accueil");
+    }
+
+    link2 = ()=>{
+        console.log("Galerie");
+    }
+
+    link3 = ()=>{
+        console.log("Contact");
+    }
     render() {
         return (
             <div className="navigation">
-                <a href="">Link 1</a>
-                <a href="">Link 2</a>
-                <a href="">Link 3</a>
+                <a onClick={this.link1}>Link 1</a>
+                <a onMouseOver={this.link2}>Link 2</a>
+                <a onDoubleClick={this.link3}>Link 3</a>
             </div>
         )
     }
